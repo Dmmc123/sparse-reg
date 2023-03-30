d9434f2cf2c0fa0024b96c6ed2afd1fc1ff07bd6
Dmmc123 <r.mihaylov@innopolis.university>
2023-03-30 21:47:20 +0400

    reg coef for losses

--- src/metrics.py
+++ src/metrics.py
@@ -35,40 +35,43 @@ def l2_norm(w: Tensor) -> Tensor:
     return torch.norm(w, p=2)
 
 
-def mean_squared_error(y_hat: Tensor, y_true: Tensor, w: Tensor = None) -> Tensor:
+def mean_squared_error(y_hat: Tensor, y_true: Tensor, w: Tensor = None, lambda_: float = 0) -> Tensor:
     """
     Calculate MSE on the given data
 
     :param Tensor y_hat: Predicted values for the target variables
     :param Tensor y_true: True values for the target variables
     :param Tensor w: Made for compatibility with lasso and ridge losses
+    :param float lambda_: Made for compatibility with lasso and ridge losses
     :return: MSE
     :rtype: Tensor
     """
     return torch.mean((y_hat - y_true) ** 2)
 
 
-def lasso(y_hat: Tensor, y_true: Tensor, w: Tensor) -> Tensor:
+def lasso(y_hat: Tensor, y_true: Tensor, w: Tensor, lambda_: float) -> Tensor:
     """
     Calculate lasso optimization goal value
 
     :param Tensor y_hat: Predicted values for the target variables
     :param Tensor y_true: True values for the target variables
     :param Tensor w: Weights of given regression model
+    :param float lambda_: Regularization coefficient
     :return: Lasso optimization goal w.r.t. current weights
     :rtype: Tensor
     """
-    return mean_squared_error(y_hat, y_true) + l1_norm(w)
+    return mean_squared_error(y_hat, y_true) + lambda_ * l1_norm(w)
 
 
-def ridge(y_hat: Tensor, y_true: Tensor, w: Tensor) -> Tensor:
+def ridge(y_hat: Tensor, y_true: Tensor, w: Tensor, lambda_: float) -> Tensor:
     """
     Calculate ridge optimization goal value
 
     :param Tensor y_hat: Predicted values for the target variables
     :param Tensor y_true: True values for the target variables
     :param Tensor w: Weights of given regression model
+    :param float lambda_: Regularization coefficient
     :return: Ridge optimization goal w.r.t. current weights
     :rtype: Tensor
     """
-    return mean_squared_error(y_hat, y_true) + l2_norm(w)
+    return mean_squared_error(y_hat, y_true) + lambda_ / 2 * l2_norm(w)
